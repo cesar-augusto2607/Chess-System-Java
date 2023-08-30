f86c1ce1f98d342293a40d7a8ea9043d10f4a25c
Cesar Augusto <cesaraugustoc@hotmail.com>
2023-08-30 17:57:29 -0300

    Logic checkmate

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,8 +16,9 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
-		List<ChessPiece> captured = new ArrayList<> ();		
-		while (true) {
+		List<ChessPiece> captured = new ArrayList<> ();	
+		
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -48,5 +49,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
